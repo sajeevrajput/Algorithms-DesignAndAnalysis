a04fadb4ff844217ab6b324a6e7e00f29964d024
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2016-09-15 23:52:22 +0530

    Update CountInversions.c
    3rd draft.. count needs to be fixed...Incomplete

--- CountInversions.c
+++ CountInversions.c
@@ -9,36 +9,36 @@
 #include <stdio.h>
 int count=0;
 
-main(){
 
-	int arr[]={16,15,14,13,20,19,18,17};
+main(){
+	int arr[]={2,4,1,3,5};
 	int len=sizeof(arr)/sizeof(int);
 
 	countInversions(arr,len);
-	printf("The number of inversions are %d: ",count);
+	printf("The number of inversions are: %d ",count);
 
 	return;
 
 }
 
-void countInversions(int *arr,int arrlen){
+countInversions(int *arr,int arrlen){
 
 	int *temp=malloc(sizeof(int)*arrlen);
-	count=mergeSortCountInv(arr,temp,0,arrlen-1);
+	mergeSortCountInv(arr,temp,0,arrlen-1);
 
 	}
 
-int mergeSortCountInv(int *arr,int *temp, int p,int r){
+mergeSortCountInv(int *arr,int *temp, int p,int r){
 	if(p<r){
 		int q=(p+r)/2;
-		count+=mergeSortCountInv(arr,temp,p,q);
-		count+=mergeSortCountInv(arr,temp,q+1,r);
-		count+=mergeCount(arr,temp,p,q,r);
+		mergeSortCountInv(arr,temp,p,q);
+		mergeSortCountInv(arr,temp,q+1,r);
+		mergeCount(arr,temp,p,q,r);
 	}
 	return count;
 }
 
-int mergeCount(int *arr,int *temp,int p,int q,int r){
+mergeCount(int *arr,int *temp,int p,int q,int r){
 	int i=p,j=q+1,k=0;
 
 	while((i<=q)&&(j<=r)){
@@ -50,12 +50,7 @@ int mergeCount(int *arr,int *temp,int p,int q,int r){
 			temp[k++]=arr[j++];
 		}
 	}
-	return count;
+	return;
 
 }
-
-
-
-
-
 
