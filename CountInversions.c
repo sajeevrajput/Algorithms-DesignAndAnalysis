5a840526ee489ca8dac6d10bae63256c760fd6ec
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2016-09-16 12:43:07 +0530

    Update CountInversions.c
    garbage value for last element fixed... Incomplete

--- CountInversions.c
+++ CountInversions.c
@@ -15,7 +15,7 @@ main(){
 	int len=sizeof(arr)/sizeof(int);
 
 	countInversions(arr,len);
-	printf("The number of inversions are: %d ",count);
+	printf("\nThe number of inversions are: %d ",count);
 
 	return;
 
@@ -26,6 +26,11 @@ countInversions(int *arr,int arrlen){
 	int *temp=malloc(sizeof(int)*arrlen);
 	mergeSortCountInv(arr,temp,0,arrlen-1);
 
+	printf("\n Elements of arr: ");
+	disp(arr,arrlen);
+	printf("\n Elements of temp: ");
+	disp(temp,arrlen);
+
 	}
 
 mergeSortCountInv(int *arr,int *temp, int p,int r){
@@ -35,7 +40,7 @@ mergeSortCountInv(int *arr,int *temp, int p,int r){
 		mergeSortCountInv(arr,temp,q+1,r);
 		mergeCount(arr,temp,p,q,r);
 	}
-	return count;
+	return;
 }
 
 mergeCount(int *arr,int *temp,int p,int q,int r){
@@ -50,7 +55,16 @@ mergeCount(int *arr,int *temp,int p,int q,int r){
 			temp[k++]=arr[j++];
 		}
 	}
+
+	while(i<=q)temp[k++]=arr[i++];
+	while(j<=r)temp[k++]=arr[j++];
 	return;
 
+}
+
+
+disp(int *arr,int len){
+	for(int i=0;i<len;i++)
+		printf(" %d",arr[i]);
 }
 
