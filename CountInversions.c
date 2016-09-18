66840f34b474119ce60ae58276a39c5fc17b8653
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2016-09-18 22:01:09 +0530

    Update CountInversions.c
    Fixed existing errors.Completed.

--- CountInversions.c
+++ CountInversions.c
@@ -1,10 +1,18 @@
-/**********************************************************************
-* PROGRAM TO COUNT THE NUMBER OF INVERSIONS IN AN ARRAY OF INTEGERS   *
-*                                                                     *
-*Authored by:   Sajeev R                                              *
-*Created on:    14th Sep, 2016                                        *
-*Completed on:                                                        *
-**********************************************************************/
+/************************************************************************
+* PROGRAM TO COUNT THE NUMBER OF INVERSIONS IN AN ARRAY OF INTEGERS   	*
+*                                                                     	*
+*	Inversions are counted by piggybacking the count variable in 	*
+*	MergeSort(O(nlogn)) rather than using a layman's O(n^2)timed	*
+*	algorithm							* 
+*	IMPORTANT:							*
+* 		The variable size here is INT which can take only 16-bit*
+* 		size. Use big data type like unsigned long int (and 	*
+* 		format specifier %ul) to get the desired result.	*
+*									*									*
+*Authored by:   Sajeev R                                              	*
+*Created on:    14th Sep, 2016                                        	*
+*Completed on:  18th Sep, 2016                                        	*
+************************************************************************/
 
 #include <stdio.h>
 int count=0;
@@ -12,35 +20,28 @@ int len=0;
 
 
 main(){
-	//int arr[]={11,10,15,8,13,16,4,12,14,1,7,6,9,0,5,3};
-	int arr[]={4,1,5,3};
-	//int arr[]={22,88,61,0,29,55,33,5,82,12};
+	int arr[]={1, 20, 6, 4, 5};
+	//int arr[]={};
 	len=sizeof(arr)/sizeof(int);
-
 	countInversions(arr,len);
 	printf("\nThe number of inversions are: %d ",count);
-
 	return;
-
 }
 
-countInversions(int arr[],int arrlen){
+countInversions(int arr[],int arrlen){	//CALLER FUNCTION to MERGE SORT
 
-	printf("\n Elements of arr: ");
-		disp(arr,arrlen);
+	printf("\n Before Sorted: ");
+	disp(arr,arrlen);
+	
 	int *temp=malloc(sizeof(int)*arrlen);
 	mergeSortCountInv(arr,temp,0,arrlen-1);
 
-	printf("\n Elements of temp: ");
+	printf("\n After Sorted: ");
 	disp(temp,arrlen);
 	}
 
-mergeSortCountInv(int arr[],int temp[], int p,int r){
-	printf("ARRAY");
-	disp(arr,len);
-	printf("TEMPORARY");
-	disp(temp,len);
-
+mergeSortCountInv(int arr[],int temp[], int p,int r){	//RECURSIVELY CALL MERGESORT
+							//AND MERGE THEM
 	int q=0;
 	if(p<r){
 		q=(p+r)/2;
@@ -52,13 +53,6 @@ mergeSortCountInv(int arr[],int temp[], int p,int r){
 }
 
 mergeCount(int arr[],int temp[],int p,int q,int r){
-
-	printf("BEFORE merge:***********************");
-	printf("\n arr");
-	disp(arr,len);
-	printf(" temp");
-	disp(temp,len);
-
 	int i=p,j=q+1,k=0;
 
 	while((i<=q)&&(j<=r)){
@@ -66,42 +60,29 @@ mergeCount(int arr[],int temp[],int p,int q,int r){
 			temp[k++]=arr[i++];
 		}
 		else{
-			count+=(q-i)+1;
+			count+=(q-i)+1;			//calculate the number of inversions
 			temp[k++]=arr[j++];
 		}
 	}
 
-	while(i<=q)temp[k++]=arr[i++];
+	while(i<=q)temp[k++]=arr[i++];	//Copy the remaining arr values if any
 	while(j<=r)temp[k++]=arr[j++];
 
-
-	for(i=p;i<=r;i++){ //else while merging sub arrays in arr[] wont be sorted
-		arr[i]=temp[i];
-		//printf("%d",i); //print array index
-	}
-
-
-
-	printf("\nAFTER merge:");
-	printf("\n arr");
-	disp(arr,len);
-	printf(" temp");
-	disp(temp,len);
-	printf("**************************************\n");
-
+	for(i=p;i<=r;i++)	/*else while merging sub arrays in arr[] wont be sorted	*/
+		arr[i]=temp[i-p];
 
 	return;
-
 
 }
 
 
-disp(int a[],int len){
+disp(int a[],int len){				//DISPLAY FUNCTION TO PRINT ELEMENTS OF AN ARRAY
 	printf("\n");
 	for(int i=0;i<len;i++)
 		printf(" %d",a[i]);
 	printf("\n");
 }
 
+//2407905288
 
 
