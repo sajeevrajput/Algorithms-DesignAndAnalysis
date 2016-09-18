178243a365715e62f4ff5158a18d92bfc3e7edbf
Sajeev Singh Rajput <sajeev.rajput@gmail.com>
2016-09-18 20:18:09 +0530

    Update CountInversions.c
    incomplete code... with debug

--- CountInversions.c
+++ CountInversions.c
@@ -8,12 +8,14 @@
 
 #include <stdio.h>
 int count=0;
+int len=0;
 
 
 main(){
-	//int arr[]={4,1,7,6,9,0,5,3};
+	//int arr[]={11,10,15,8,13,16,4,12,14,1,7,6,9,0,5,3};
 	int arr[]={4,1,5,3};
-	int len=sizeof(arr)/sizeof(int);
+	//int arr[]={22,88,61,0,29,55,33,5,82,12};
+	len=sizeof(arr)/sizeof(int);
 
 	countInversions(arr,len);
 	printf("\nThe number of inversions are: %d ",count);
@@ -22,38 +24,45 @@ main(){
 
 }
 
-countInversions(int *arr,int arrlen){
+countInversions(int arr[],int arrlen){
 
 	printf("\n Elements of arr: ");
 		disp(arr,arrlen);
-	printf("\n\n");
-
 	int *temp=malloc(sizeof(int)*arrlen);
 	mergeSortCountInv(arr,temp,0,arrlen-1);
 
-
 	printf("\n Elements of temp: ");
 	disp(temp,arrlen);
-	printf("\n\n");
 	}
 
-mergeSortCountInv(int *arr,int *temp, int p,int r){
+mergeSortCountInv(int arr[],int temp[], int p,int r){
+	printf("ARRAY");
+	disp(arr,len);
+	printf("TEMPORARY");
+	disp(temp,len);
+
+	int q=0;
 	if(p<r){
-		int q=(p+r)/2;
+		q=(p+r)/2;
 		mergeSortCountInv(arr,temp,p,q);
 		mergeSortCountInv(arr,temp,q+1,r);
 		mergeCount(arr,temp,p,q,r);
-		//disp(temp,r-p+1);
-		//printf("\n");
 	}
 	return;
 }
 
-mergeCount(int *arr,int *temp,int p,int q,int r){
+mergeCount(int arr[],int temp[],int p,int q,int r){
+
+	printf("BEFORE merge:***********************");
+	printf("\n arr");
+	disp(arr,len);
+	printf(" temp");
+	disp(temp,len);
+
 	int i=p,j=q+1,k=0;
 
 	while((i<=q)&&(j<=r)){
-		if(arr[i]<arr[j]){
+		if(arr[i]<=arr[j]){
 			temp[k++]=arr[i++];
 		}
 		else{
@@ -63,21 +72,23 @@ mergeCount(int *arr,int *temp,int p,int q,int r){
 	}
 
 	while(i<=q)temp[k++]=arr[i++];
-		while(j<=r)temp[k++]=arr[j++];
+	while(j<=r)temp[k++]=arr[j++];
 
 
-	for(i=p;i<=r;i++) //else while merging sub arrays in arr[] wont be sorted
+	for(i=p;i<=r;i++){ //else while merging sub arrays in arr[] wont be sorted
 		arr[i]=temp[i];
+		//printf("%d",i); //print array index
+	}
 
 
 
+	printf("\nAFTER merge:");
+	printf("\n arr");
+	disp(arr,len);
+	printf(" temp");
+	disp(temp,len);
+	printf("**************************************\n");
 
-
-	disp(temp,r-p+1);
-	printf("TEMP\n");
-
-	disp(arr,r-p+1);
-	printf("ARR\n");
 
 	return;
 
@@ -85,9 +96,11 @@ mergeCount(int *arr,int *temp,int p,int q,int r){
 }
 
 
-disp(int *arr,int len){
+disp(int a[],int len){
+	printf("\n");
 	for(int i=0;i<len;i++)
-		printf(" %d",arr[i]);
+		printf(" %d",a[i]);
+	printf("\n");
 }
 
 
